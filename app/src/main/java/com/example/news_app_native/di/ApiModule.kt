df5c3d1c529e6090c2f1bfe2def322071b676465
aretka <aretask2@gmail.com>
2022-05-06 19:01:01 +0300

    Provide newsapi with hilt

--- app/src/main/java/com/example/news_app_native/di/ApiModule.kt
+++ app/src/main/java/com/example/news_app_native/di/ApiModule.kt
@@ -1,4 +1,31 @@
 package com.example.news_app_native.di
 
-class ApiModule {
+import com.example.news_app_native.network.NewsApi
+import com.example.news_app_native.network.NewsApiInterceptor
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    private const val BASE_URL = "https://newsapi.org"
+
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient().newBuilder()
+                    .addInterceptor(NewsApiInterceptor())
+                    .build()
+            )
+            .build()
+            .create(NewsApi::class.java)
+    }
 }
