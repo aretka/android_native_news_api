062425572300576d98cf1f1036ba4f7bed5f4a4b
aretka <aretask2@gmail.com>
2022-05-07 22:21:06 +0300

    Map to new list when fetched

--- app/src/main/java/com/example/news_app_native/network/Repository.kt
+++ app/src/main/java/com/example/news_app_native/network/Repository.kt
@@ -1,17 +1,20 @@
 package com.example.news_app_native.network
 
-import android.util.Log
 import com.example.news_app_native.network.models.Article
-import com.example.news_app_native.network.models.Articles
 import javax.inject.Inject
 
 class Repository @Inject constructor(
     private val newsApi: NewsApi
 ) {
-    suspend fun getArticles() : List<Article> {
+    suspend fun getArticles(): List<Article> {
         val response = newsApi.getTopHeadlines()
-        Log.d(TAG, "getArticles response: ${response.code()}")
-        return response.body()?.articles ?: Articles().articles
+        var articles: List<Article> = emptyList()
+        if (response.body() != null) {
+            articles = response.body()!!.articles.map {
+                it.withFormattedDate()
+            }
+        }
+        return articles
     }
 
     companion object {
