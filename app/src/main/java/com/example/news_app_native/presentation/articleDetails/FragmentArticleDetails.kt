61a8a20a6e58993af99d1029f03868e0cf0e80f9
aretka <aretask2@gmail.com>
2022-05-07 22:23:22 +0300

    Implement article details screen

--- app/src/main/java/com/example/news_app_native/presentation/articleDetails/FragmentArticleDetails.kt
+++ app/src/main/java/com/example/news_app_native/presentation/articleDetails/FragmentArticleDetails.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.news_app_native.R
 import com.example.news_app_native.databinding.FragmentArticleDetailsBinding
+import com.example.news_app_native.network.models.Article
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FragmentArticleDetails: Fragment() {
+class FragmentArticleDetails : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,8 +21,23 @@ class FragmentArticleDetails: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = FragmentArticleDetailsBinding.inflate(inflater, container, false)
+        val article = FragmentArticleDetailsArgs.fromBundle(requireArguments()).article
+        binding.setUpUI(article)
+
         return binding.root
     }
 
+    private fun FragmentArticleDetailsBinding.setUpUI(article: Article) {
+        Glide.with(requireContext())
+            .load(article.urlToImage)
+            .skipMemoryCache(true)
+            .placeholder(R.drawable.ic_downloading)
+            .error(R.drawable.ic_broken_image)
+            .into(articlePicture)
+        author.text = article.author ?: "No author"
+        date.text = article.publishedAt.toString() ?: "No date"
+        title.text = article.title ?: "No title"
+        content.text = article.content ?: "No content"
+    }
 
 }
