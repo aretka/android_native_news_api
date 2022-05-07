7c8f54ec39d3203433f3087f3408737f2a9c5baa
aretka <aretask2@gmail.com>
2022-05-07 22:22:00 +0300

    Format date

--- app/src/main/java/com/example/news_app_native/network/models/Article.kt
+++ app/src/main/java/com/example/news_app_native/network/models/Article.kt
@@ -1,5 +1,14 @@
 package com.example.news_app_native.network.models
 
+import android.os.Parcelable
+import android.util.Log
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@Parcelize
 data class Article(
     val author: String? = null,
     val content: String? = null,
@@ -8,4 +17,13 @@ data class Article(
     val title: String? = null,
     val url: String? = null,
     val urlToImage: String? = null
-)
+) : Parcelable {
+    fun withFormattedDate(): Article {
+        if(publishedAt != null) {
+            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            val date = ZonedDateTime.parse(publishedAt).format(formatter)
+            return this.copy(publishedAt = date)
+        }
+        return this
+    }
+}
